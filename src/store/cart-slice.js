560a19db4b2e43bc846ec97fb80fc6be940ce162
cibss <cibss13@gmail.com>
2022-09-02 17:32:05 +0700

    action creator thunk

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,16 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [],
     totalQuantity: 0,
-    changed: false
   },
   reducers: {
     addItemToCart(state, action) {
       const newItem = action.payload;
-      const existingItem = state.items.find(item => item.id === newItem.id);
+      const existingItem = state.items.find((item) => item.id === newItem.id);
 
       state.totalQuantity++;
 
@@ -20,29 +20,73 @@ const cartSlice = createSlice({
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
-          name: newItem.title
-        })
+          name: newItem.title,
+        });
       } else {
         existingItem.quantity++;
-        existingItem.totalPrice += newItem.price
+        existingItem.totalPrice += newItem.price;
       }
     },
     removeItemFromCart(state, action) {
       const id = action.payload;
-      const existingItem = state.items.find(item => item.id === id);
+      const existingItem = state.items.find((item) => item.id === id);
 
       state.totalQuantity--;
-      state.changed = true;
 
       if (existingItem.quantity === 1) {
-        state.items = state.items.filter(item => item.id !== id)
+        state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
-        existingItem.totalPrice -= existingItem.price
+        existingItem.totalPrice -= existingItem.price;
       }
+    },
+  },
+});
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "sending",
+        title: "Pending",
+        message: "Sending cart data...",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-api-c480c-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed",
+        })
+      );
     }
-  }
-})
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
